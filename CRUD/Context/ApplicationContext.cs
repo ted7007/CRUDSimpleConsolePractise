d010047eefca2b4c5ace813513ea2bfa4faa526f
Александр Косилов <ted7007@yandex.ru>
2022-04-22 12:37:18 +1000

    Created prototype of CRUD

---
+++ CRUD/Context/ApplicationContext.cs
@@ -0,0 +1,33 @@
+﻿using CRUD.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Context
+{
+    /// <summary>
+    /// Взаимодействие с базой данных в Entity Framework Core происходит посредством специального класса - контекста данных. 
+    ///  DbContext определяет контекст данных, используемый для взаимодействия с базой данных
+    /// </summary>
+    public class ApplicationContext : DbContext
+    {
+        /// <summary>
+        /// представляет набор объектов, которые хранятся в базе данных
+        /// </summary>
+        public DbSet<Student> Students => Set<Student>();
+
+        public ApplicationContext() => Database.EnsureCreated();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="optionsBuilder">устанавливает параметры подключения</param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;user id=root;persistsecurityinfo=True;database=students;password=0802");
+        }
+    }
+}
---
+++ CRUD/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using CRUD.View;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.EntityFrameworkCore;
+
+
+namespace CRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CRUDView view = new CRUDView();
+            view.Start();
+        }
+    }
+
+}
+
+
